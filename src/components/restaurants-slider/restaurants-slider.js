5aa2ba06c97c0fad156b35e788e12aab237b8701
mizza-cz <mizza.cz666@gmail.com>
2022-10-09 13:50:57 +0200

    add foodcourt

---
+++ src/components/restaurants-slider/restaurants-slider.js
@@ -0,0 +1,51 @@
+$(".c-restaurant-slider .js-slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  // speed: 3000,
+  // autoplay: true,
+  prevArrow:
+    '<button class="slider__btn slider__btnprev"><img src="images/ico/left-slider.svg" alt="" ></button> ',
+  nextArrow:
+    ' <button class="slider__btn  slider__btnnext"><img src="images/ico/right-slider.svg" alt = "" ></button>',
+  responsive: [
+    {
+      breakpoint: 1641,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 641,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      },
+    },
+  ],
+});
