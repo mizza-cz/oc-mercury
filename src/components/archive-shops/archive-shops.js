ae8c8a1a5dd2244c23b9bb373dd8d87b192f0ed4
mizza-cz <mizza.cz666@gmail.com>
2022-10-09 16:38:42 +0200

    add shop archive

---
+++ src/components/archive-shops/archive-shops.js
@@ -0,0 +1,5 @@
+// document
+//   .querySelectorAll(".c-archive-shops__form-list input[type=checkbox]")
+//   .forEach((input) =>
+//     input.addEventListener("change", () => input.form.submit())
+//   );
