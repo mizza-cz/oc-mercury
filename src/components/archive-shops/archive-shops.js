13af0cf5da373956112212a2e9116afe75888cf5
mizza-cz <mizza.cz666@gmail.com>
2022-10-12 10:21:21 +0200

    add fixes

--- src/components/archive-shops/archive-shops.js
+++ src/components/archive-shops/archive-shops.js
@@ -3,3 +3,7 @@
 //   .forEach((input) =>
 //     input.addEventListener("change", () => input.form.submit())
 //   );
+$(".c-archive-shops__form-filter").on("click", function () {
+  $(this).toggleClass("c-archive-shops__form-filter--closed");
+  $(this).next().slideToggle();
+});
